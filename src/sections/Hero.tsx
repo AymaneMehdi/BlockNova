983c014e6c670aa5c22a0fc1be279f7eb0b0e998
Aymane Mehdi <unlocked.dark.web@gmail.com>
2025-07-26 22:16:00 +0100

    Error : images url

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -28,25 +28,25 @@ export const HeroSection = () => {
             </div>
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <Circle className="absolute left-[200px] -top-[900px]">
-                <img src="/assets/images/cube.png" alt="Cube 3D image" className="size-[140px]" />
+                <img src="assets/images/cube.png" alt="Cube 3D image" className="size-[140px]" />
               </Circle>
             </div>
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <Circle className="absolute left-[200px] top-[270px]">
-                <img src="/assets/images/cuboid.png" alt="Cuboid 3D image" className="size-[140px]" />
+                <img src="assets/images/cuboid.png" alt="Cuboid 3D image" className="size-[140px]" />
               </Circle>
             </div>
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <Circle className="absolute -left-[600px] -top-[80px]">
-                <img src="/assets/images/torus.png" alt="Cuboid 3D image" className="size-[140px]" />
+                <img src="assets/images/torus.png" alt="Cuboid 3D image" className="size-[140px]" />
               </Circle>
             </div>
             <img
-              src="/assets/images/icosahedron.png"
+              src="assets/images/icosahedron.png"
               alt=""
               className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 saturate-[10%] brightness-[4%] hue-rotate-[240deg]" />
             <img
-              src="/assets/images/icosahedron.png"
+              src="assets/images/icosahedron.png"
               alt="Icosahedron 3D Image"
               className="w-[500px]"
             />
