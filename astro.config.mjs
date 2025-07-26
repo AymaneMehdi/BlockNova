237cd8a53d0247f8b9f2ccfd8346f736973535e9
Aymane Mehdi <unlocked.dark.web@gmail.com>
2025-07-26 20:39:37 +0100

    update Astro config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import github from "@astrojs/github";
 
 export default defineConfig({
-  site: "https://aymanemehdi.github.io/BlockNova", 
-  base: "/BlockNova",                              
+  site: "https://aymanemehdi.github.io/BlockNova",
+  base: "/BlockNova",
   trailingSlash: "always",
   integrations: [
     tailwind({ applyBaseStyles: false }),
     react(),
-    github(),
   ],
 });
-
