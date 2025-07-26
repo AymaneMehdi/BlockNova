51cee4f6b76384253b8f72a9e5ef72bbaf90a7b2
Aymane Mehdi <unlocked.dark.web@gmail.com>
2025-07-26 19:48:12 +0100

    update Astro config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://USERNAME.github.io/REPO",
+  base: "/REPO",
+  trailingSlash: "always",
   integrations: [
     tailwind({
       applyBaseStyles: false,
